e8d5fe5f42cd2433c36d69aafe5740f3971fa391
sunwoolee <51154256+sunwoolee@users.noreply.github.com>
2023-08-28 14:47:07 +0900

    Update quant.py

--- quant.py
+++ quant.py
@@ -1,5 +1,5 @@
 import torch
-from .major import linear_quantize, custom_fp_quantize, fp_quantize, linear_hysteresis, custom_fp_hysteresis, fp_hysteresis, binary_quantize, binary_hysteresis, ternary_quantize, ternary_hysteresis
+from .major import linear_quantize, custom_fp_quantize, fp_quantize, linear_hysteresis, custom_fp_hysteresis, fp_hysteresis, binary_quantize, binary_hysteresis, ternary_quantize, ternary_hysteresis, linear_block_quantize, linear_ch_block_quantize
 
 class qformat():
     def __init__(self, format_type):
@@ -15,6 +15,21 @@ class binary_format(qformat):
 class ternary_format(qformat):
     def __init__(self):
         super().__init__('ternary')
+
+class linear_block_format(qformat):
+    def __init__(self, bit_num, scale_man, scale_log2=False, ):
+        super().__init__('linear_block')
+        self.bit_num = bit_num
+        self.scale_log2 = scale_log2
+        self.scale_man = scale_man
+
+class linear_ch_block_format(qformat):
+    def __init__(self, bit_num, scale_man, scale_log2=False, ch_scale_bit=None):
+        super().__init__('linear_ch_block')
+        self.bit_num = bit_num
+        self.scale_log2 = scale_log2
+        self.scale_man = scale_man
+        self.ch_scale_bit = ch_scale_bit
 
 class linear_format(qformat):
     def __init__(self, bit_num, unsigned=False):
@@ -32,6 +47,14 @@ class custom_fp_format(qformat):
         self.man = torch.tensor(man, dtype=torch.int)
         self.scale_diff = man[0]
 
+class custom_fp_multi_type_format(qformat):
+    def __init__(self, man_list, default_man):
+        super().__init__('custom_fp_multi_type')
+        self.man = []
+        for man in man_list:
+            self.man.append(torch.tensor(man, dtype=torch.int))
+        self.default_man = torch.tensor(default_man, dtype=torch.int)
+        
 class fp_format(qformat):
     def __init__(self, exp_bit, man_bit, bias=None):
         super().__init__('fp')
@@ -43,7 +66,7 @@ class fp_format(qformat):
             self.bias = bias
 
 class quant():
-    def __init__(self, qformat, room=0, tracking=True, stochastic=False, ch_wise=False, ch_dim=0):
+    def __init__(self, qformat, room=0, tracking=True, stochastic=False, ch_wise=False, ch_dim=0, block_wise=False, block_size=0, block_dim=0):
         """ use accurate scale if tracking is False  """
         self._type = qformat.get_type()
         self.qformat = qformat
@@ -52,6 +75,9 @@ class quant():
         self.stochastic = stochastic
         self.ch_wise = ch_wise
         self.ch_dim = ch_dim
+        self.block_wise = block_wise
+        self.block_size = block_size
+        self.block_dim = block_dim
         if isinstance(ch_dim, list):
             tmp = -1
             for ch in ch_dim:
@@ -59,13 +85,13 @@ class quant():
                     raise Exception("When ch_dim is list object, elements must be in ascending order")
                 tmp = ch
 
-    def quantize(self, input, scale=None, room=None):
+    def quantize(self, input, scale=None, room=None, quant_type=None):
         if self.tracking is False:
             scale = None
         if room is None:
             room = self.room
         if self._type == 'linear':
-            return linear_quantize(input, scale, self.qformat.bit_num, room, self.stochastic, self.ch_wise, self.ch_dim, self.qformat.unsigned)
+            return linear_quantize(input, scale, self.qformat.bit_num, room, self.stochastic, self.ch_wise, self.ch_dim, self.block_wise, self.block_size, self.block_dim, self.qformat.unsigned)
         elif self._type == 'custom_fp':
             return custom_fp_quantize(input, scale, self.qformat.man.to(input.device), room, self.stochastic, self.ch_wise, self.ch_dim)
         elif self._type == 'fp':
@@ -74,6 +100,18 @@ class quant():
             return binary_quantize(input, self.stochastic, self.ch_wise, self.ch_dim)
         elif self._type == 'ternary':
             return ternary_quantize(input, self.stochastic, self.ch_wise, self.ch_dim)
+        elif self._type == 'linear_block':
+            return linear_block_quantize(input, self.qformat.bit_num, self.block_size, self.block_dim, self.qformat.scale_log2, self.qformat.scale_man)
+        elif self._type == 'linear_ch_block':
+            return linear_ch_block_quantize(input, self.qformat.bit_num, self.block_size, self.block_dim, self.qformat.scale_log2, self.qformat.scale_man, self.qformat.ch_scale_bit)
+        elif self._type == 'custom_fp_multi_type':
+            if quant_type == None:
+                raise Exception("quant type is None!")
+            elif quant_type == -1:
+                man = self.qformat.default_man.to(input.device)
+            else:
+                man = self.qformat.man[quant_type].to(input.device)
+            return custom_fp_quantize(input, scale, man, room, self.stochastic, self.ch_wise, self.ch_dim)
         
     def hysteresis(self, pre_input, input, scale=None, room=None):
         if self.tracking is False:
@@ -90,3 +128,5 @@ class quant():
             return binary_hysteresis(pre_input, input, self.ch_wise, self.ch_dim)
         elif self._type == 'ternary':
             return ternary_hysteresis(pre_input, input, self.ch_wise, self.ch_dim)
+        elif self._type == 'linear_block':
+            raise Exception("hysteresis for linear_block is not implemented.")
